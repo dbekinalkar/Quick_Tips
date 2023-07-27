b6bb73c5a8ee2ee1c1bd7b0ba7e90418841e384a
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-07-27 10:08:37 -0500

    Update to include all variables

--- lib/providers/tip_provider.dart
+++ lib/providers/tip_provider.dart
@@ -2,12 +2,22 @@ import 'package:flutter/foundation.dart';
 import 'package:quick_tips/models/tip_calculator.dart';
 
 class TipProvider with ChangeNotifier {
-  final TipCalculator _tipCalculator = TipCalculator();
+  TipCalculator _tipCalculator = TipCalculator();
 
   TipCalculator get tipCalculator => _tipCalculator;
 
   void updateBillAmount(double billAmount) {
     _tipCalculator.billAmount = billAmount;
+    notifyListeners();
+  }
+
+  void updateTipPercentage(double tipPercentage) {
+    _tipCalculator.tipPercentage = tipPercentage;
+    notifyListeners();
+  }
+
+  void updateSplitBy(int splitBy) {
+    _tipCalculator.splitBy = splitBy;
     notifyListeners();
   }
 }
