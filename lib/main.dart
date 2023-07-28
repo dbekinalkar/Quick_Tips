e8b6be01190b24e446e8a30ccb3b4e6958fb81f0
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-07-28 08:15:26 -0500

    Implement Calculation Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_tips/providers/tip_provider.dart';
 import 'package:quick_tips/screens/home_screen.dart';
+import 'package:quick_tips/screens/tip_calculation_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -18,7 +21,8 @@ class MyApp extends StatelessWidget {
         darkTheme: ThemeData.dark(),
         initialRoute: '/',
         routes: {
-          '/': (context) => HomeScreen(),
+          '/': (context) => const HomeScreen(),
+          '/tipCalculation': (context) => const TipCalculationScreen(),
         },
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:quick_tips/widgets/tip_calculator_form.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QuickTips - Home'),
+        title: const Text('QuickTips - Home'),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Column(
           children: [
             TipCalculatorForm(),
